4ea8dd6d6a33cfc631a184166e34c8d808ac6957
Chatchai Saratakij <CSaratakij@gmail.com>
2019-04-07 17:58:01 +0700

    Add : Finish example

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    Sprite[] characterSprites;
+
+
+    SpriteRenderer spriteRenderer;
+
+
+    void Awake()
+    {
+        Initialize();
+        LoadCharacterPreference();
+    }
+
+    void Update()
+    {
+        InputHandler();
+    }
+
+    void Initialize()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void LoadCharacterPreference()
+    {
+        spriteRenderer.sprite = characterSprites[Global.SelectCharacterIndex];
+    }
+
+    void InputHandler()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
+
---
+++ Assets/Scripts/UI/UICharacterSelection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UICharacterSelection : MonoBehaviour
+{
+    public void OnSelectCharacter(int index)
+    {
+        Global.SelectCharacterIndex = index;
+    }
+
+    public void OnClickPlay()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
+
